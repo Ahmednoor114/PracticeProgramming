4d9f63255ff4b48efd81a8ed0b103ca257b8bd41
Ahmednoor114 <galangale1@gmail.com>
2022-04-30 16:18:36 -0500

    Created Method Classes

---
+++ src/main/java/com/sg/foundations/flowcontrol/methods/MethodToTheMadness.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.foundations.flowcontrol.methods;
+
+/**
+ *
+ * @author 16127
+ */
+public class MethodToTheMadness {
+    public static void main(String[] args) {
+        eatMe();
+        drinkMe();
+        
+        System.out.println("\n - Lewis Carroll, Alice in Wonderland");
+    }
+    
+    static void eatMe(){
+        System.out.println(" 'But I dont want to go among mad people, ' Alice remarked.");
+        System.out.println(" 'Oh, you can't help that, ' said the Cat.");
+        System.out.println(" 'We're all mad here. I'm mad. You're mad. ' ");
+    }
+    
+    static void drinkMe(){
+        System.out.println(" 'How do you kmow I'm mad?' said Alice.");
+        System.out.println(" 'You must be,' said the Cat, ' or you would'nt have come here. ' ");
+        
+    }
+    
+}
